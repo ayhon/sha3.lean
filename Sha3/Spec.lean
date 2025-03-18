5cec60b253db979357fef2dd30ecf740dd8eca37
ayhon <ayhonnick@gmail.com>
2025-03-18 15:26:11 +0100

    feat: SHA3 specification

--- Sha3/Spec.lean
+++ Sha3/Spec.lean
@@ -2,32 +2,50 @@
 private def Vector.xor(v: Vector Bool n): Bool := v.foldl (·^^·) false
 private def Vector.modify(v: Vector α n)(i: Fin n)(modif: α → α) : Vector α n :=
   ⟨v.toArray.modify i modif, by simp only [Array.size_modify, size_toArray] ⟩
+private def Vector.adjust(acc: Vector α n)(len: Nat)(fill: α): Vector α len := 
+  let res := acc.toArray.take len ++ (Array.mkArray (len - n) fill)
+  ⟨res, by 
+    simp [res]
+    cases h: decide (len > n) <;> simp at *
+    · simp [Nat.min_eq_left h]
+      exact Nat.sub_eq_zero_iff_le.mpr h
+    · simp [Nat.min_eq_right (Nat.le_of_lt h)]
+      simp [←Nat.add_sub_assoc (Nat.le_of_lt h) n]
+      simp [Nat.add_sub_cancel_left]
+    ⟩
+private def Vector.ofArray(arr: Array α): Vector α arr.size := ⟨arr, rfl⟩
 
-local instance: OfNat Bool 0 where
-  ofNat := false
-local instance: OfNat Bool 1 where
-  ofNat := true
+private def Array.chunks(k: Nat)(arr: Array α): Array (Vector α k) :=
+  if h: arr.size < k ∨ k = 0 then 
+    #[]
+  else
+    #[⟨arr.extract (stop := k), by simp_all [Nat.min_eq_left]⟩] ++ (arr.extract (start := k)).chunks k
+termination_by arr.size
 
-namespace Spec
+local instance: OfNat Bool 0 where ofNat := false
+local instance: OfNat Bool 1 where ofNat := true
+
+namespace Spec/- {{{ -/
 abbrev Bit := Bool
+abbrev BitString (n: Nat) := Vector Bit n
 
-namespace KeccakP
+def BitArray.filled(n: Nat)(default: Bit) := Vector.mkVector n default
 
-variable (l: Fin 7)    -- Allowed values are:  0,  1,   2,   3,   4,   5,    6
-abbrev w := 2^l.val    -- Allowed values are:  1,  2,   4,   8,  16,  32,   64
-abbrev b := 5 * 5 * w l-- Allowed values are: 25, 50, 100, 200, 400, 800, 1600
+variable {l: Fin 7}               -- Possible values:  0,  1,   2,   3,   4,   5,    6
+abbrev w (l: Fin 7) := 2^l.val    -- Possible values:  1,  2,   4,   8,  16,  32,   64
+abbrev b (l: Fin 7) := 5 * 5 * w l-- Possible values: 25, 50, 100, 200, 400, 800, 1600
 
+namespace Keccak/- {{{ -/
 /-- The state of the KeccakP function -/
-structure StateArray where
+structure StateArray (l: Fin 7) where
   ofVector ::
-  toVector : Vector Bool (b l)
+  toVector : BitString (b l)
   deriving Inhabited
 
 namespace StateArray/- {{{ -/
-variable {l: Fin 7}
-variable (x: Fin 5)(y: Fin 5)(z: Fin (w l))
+variable (x: Fin 5)(y: Fin 5)(z: Fin (w l))(c: Fin (b l))
 
-private def decodeIndex(c: Fin (b l)): Fin 5 × Fin 5 × Fin (w l) := 
+private def decodeIndex: Fin 5 × Fin 5 × Fin (w l) := 
   -- NOTE: The order of the indices is Y, X, Z so that lanes are placed
   -- closer together in memory.
     have x_lt := by apply Nat.mod_lt; decide
@@ -58,32 +76,30 @@ private def encodeIndex: Fin (b l) :=
     exact y.isLt
   ⟨w l * (5 * y + x) + z, this⟩
 
-def map(f: Bool → Bool)(A: StateArray l): StateArray l := .ofVector <| A.toVector.map f
+def map(f: Bit → Bit)(A: StateArray l): StateArray l := .ofVector <| A.toVector.map f
 
 /-- Given a function f which takes x, y and z, return a state array A' where
     A'[x,y,z] = f x y z -/
-def ofFn(f: Fin 5 → Fin 5 → Fin (w l) → Bool): StateArray l := 
+def ofFn(f: Fin 5 → Fin 5 → Fin (w l) → Bit): StateArray l := 
   .ofVector <| Vector.ofFn fun c =>
     let (x,y,z) := decodeIndex c
     f y x z
 
-def get(A: StateArray l): Bool := Vector.get A.toVector <| encodeIndex x y z
+def get(A: StateArray l): Bit := A.toVector.get <| encodeIndex x y z
 
-/- instance: GetElem (StateArray l) (Fin 5 × Fin 5 × Fin (w l)) Bool (fun _ _ => True) where -/
+/- instance: GetElem (StateArray l) (Fin 5 × Fin 5 × Fin (w l)) Bit (fun _ _ => True) where -/
 /-   getElem | A, (x,y,z), _ => A.get x y z -/
 /- macro A:term noWs "[" xs:term,* "]" : term => `( $A[($[$xs],*)] ) -/
 
-def set(val: Bool)(A: StateArray l): StateArray l :=
-  StateArray.ofFn fun x₂ y₂ z₂ => if x₂ == x ∧ y₂ == y ∧ z₂ == z then val else A.get x y z
+def set(val: Bit)(A: StateArray l): StateArray l := .ofVector <| A.toVector.set (encodeIndex x y z) val
 
-def row (A: StateArray l): Vector Bool 5     := Vector.ofFn fun x => A.get x y z
-def col (A: StateArray l): Vector Bool 5     := Vector.ofFn fun y => A.get x y z
-def lane(A: StateArray l): Vector Bool (w l) := Vector.ofFn fun z => A.get x y z
+def row (A: StateArray l): BitString 5     := Vector.ofFn fun x => A.get x y z
+def col (A: StateArray l): BitString 5     := Vector.ofFn fun y => A.get x y z
+def lane(A: StateArray l): BitString (w l) := Vector.ofFn fun z => A.get x y z
 
 end StateArray/- }}} -/
 
 section «Step Mappings»/- {{{ -/
-variable {l: Fin 7}
 
 def θ(A: StateArray l): StateArray l := 
   StateArray.ofFn fun x y z => (A.get x y z) ^^ (A.col (x-1) z).xor ^^ (A.col (x+1) (z-1)).xor
@@ -109,9 +125,9 @@ def χ(A: StateArray l): StateArray l :=
 def ι.rc (t: Nat) := Id.run do
   let t := Fin.ofNat' 255 t
   if t = 0 then return true  
-  let mut R: Vector Bool 8 := #v[1,0,0,0, 0,0,0,0]
+  let mut R: BitString 8 := #v[1,0,0,0, 0,0,0,0]
   for i in [1:t] do
-    let R': Vector Bool 9 := ⟨#[0] ++ R.toArray, by simp⟩
+    let R': BitString 9 := ⟨#[0] ++ R.toArray, by simp⟩
     let R' := R'.modify 0 (· ^^ R'.get! 8)
     let R' := R'.modify 4 (· ^^ R'.get! 8)
     let R' := R'.modify 5 (· ^^ R'.get! 8)
@@ -140,6 +156,101 @@ def ι(iᵣ: Nat)(A: StateArray l): StateArray l := Id.run do
 
 end «Step Mappings»/- }}} -/
 
-end KeccakP
+def Rnd(A: StateArray l)(iᵣ: Nat) := ι iᵣ <| χ <| π <| ρ <| θ <| A
 
-end Spec
+def P(l: Fin 7)(nᵣ: Nat)(S: BitString (b l)): BitString (b l) := Id.run do
+  let mut A := StateArray.ofVector S
+  for iᵣ in [12 + 2*l - nᵣ:12 + 2*l - 1] do
+    A := Rnd A iᵣ
+  return A.toVector
+
+def F(l: Fin 7) := Keccak.P l (nᵣ:= 12 + 2*l)
+
+end Keccak/- }}} -/
+
+section Sponge/- {{{ -/
+
+/- section Pad -/
+
+/- /1- structure PadFn where -1/ -/
+/- /1-   -- x > 0 -1/ -/
+/- /1-   len:   (x:Nat) → (m:Nat) → Nat -1/ -/
+/- /1-   apply: (x:Nat) → (m:Nat) → BitArray (len x m) -1/ -/
+/- /1-   prop{x m}: x ∣ (len x m) -1/ -/
+
+/- end Pad -/
+
+instance: Xor (BitString n)  where
+  xor as bs := as.zipWith (·^^·) bs
+
+def sponge.squeze
+    (f: BitString (b l) → BitString (b l))
+    (r: Nat)
+    /- [NeZero r] -/
+    (Z: Array Bit)
+    (S: BitString (b l))
+: BitString d
+:= 
+    if h: Z.size >= d then
+      ⟨Z.take d, by simp [Nat.min_eq_left h]⟩
+    else
+      sponge.squeze f r (Z ++ S.toArray.take r) (f S)
+partial_fixpoint -- NOTE: Needed because without `r>0` we cannot prove termination
+-- Alternatively:
+/- termination_by d - Z.size -/
+/- decreasing_by -/
+/-   have : (b l) >= 1 :=by -/
+/-     simp [b, w] -/
+/-     apply Nat.mul_pos -/
+/-     · decide -/
+/-     · exact Nat.two_pow_pos l.val -/
+/-   rename_i r_ne_zero -/
+/-   have : r >= 1 := Nat.pos_of_ne_zero r_ne_zero.out -/
+/-   simp -/
+/-   omega -/
+
+def sponge{l: Fin 7}
+  (f: BitString (b l) → BitString (b l))
+  (pad: Nat → Nat → Array Bit)
+  (r: Nat) -- non-negative integer
+  /- [NeZero r] -/
+  ⦃m: Nat⦄
+  (N: BitString m)
+  (d: Nat) -- non-negative integer
+: BitString d := 
+  let P := N.toArray ++ pad r N.size
+  let n := P.size / r
+  let c := (b l) - r -- NOTE: Deprecated by use of `Vector.adjust`
+  let chunks := P.chunks r
+  Id.run do
+  let mut S: BitString (b l) := Vector.mkVector (b l) false
+  for i in [0:n] do
+    S := f (S ^^^ (chunks[i]!.adjust (b l) false))
+  sponge.squeze f r (S.toArray.take r) S
+
+
+-- NOTE: We need to assume `x > 2` or the modular arithmetic
+def «pad10*1»(x m: Nat): Array Bit := 
+  let j := Int.toNat <| (- m - 2) % x
+  (#v[true] ++ BitArray.filled j false ++ #v[true]).toArray
+
+end Sponge/- }}} -/
+
+def Keccak(c: Nat):= sponge (f := Keccak.P 6 (nᵣ := 24)) (pad := «pad10*1») (r := 1600 - c)
+
+private abbrev SHA3_suffix: Array Bit := #[0,1]
+private abbrev RawSHAKE_suffix: Array Bit := #[1,1]
+private abbrev SHAKE_suffix: Array Bit := RawSHAKE_suffix ++ #[1,1]
+
+def SHA3_224(M : Array Bit) := Keccak (c := 448) (Vector.ofArray <| M ++ SHA3_suffix) 224
+def SHA3_256(M : Array Bit) := Keccak (c := 512) (Vector.ofArray <| M ++ SHA3_suffix) 256
+def SHA3_384(M : Array Bit) := Keccak (c := 768) (Vector.ofArray <| M ++ SHA3_suffix) 384
+def SHA3_512(M : Array Bit) := Keccak (c :=1024) (Vector.ofArray <| M ++ SHA3_suffix) 512
+
+def SHAKE128(M : Array Bit)(d: Nat) := Keccak (c := 256) (Vector.ofArray <| M ++ SHAKE_suffix) d
+def SHAKE256(M : Array Bit)(d: Nat) := Keccak (c := 512) (Vector.ofArray <| M ++ SHAKE_suffix) d
+
+def RawSHAKE128(M : Array Bit)(d: Nat) := Keccak (c := 256) (Vector.ofArray <| M ++ RawSHAKE_suffix) d
+def RawSHAKE256(M : Array Bit)(d: Nat) := Keccak (c := 512) (Vector.ofArray <| M ++ RawSHAKE_suffix) d
+
+end Spec/- }}} -/
