7e69421d42b73fe8b268e9ba3670cb70595c0187
ayhon <ayhonnick@gmail.com>
2025-05-02 22:06:12 +0200

    refactor: remove unused definitions

--- Sha3/Utils.lean
+++ Sha3/Utils.lean
@@ -1,22 +1,7 @@
 import Init.Data.Nat.Div.Lemmas
 
-/- private def Vector.xor(v: Vector Bool n): Bool := v.foldl (·^^·) false -/
-/- private def Vector.modify(v: Vector α n)(i: Fin n)(modif: α → α) : Vector α n := -/
-/-   ⟨v.toArray.modify i modif, by simp only [Array.size_modify, size_toArray] ⟩ -/
-/- def Vector.adjust(acc: Vector α n)(len: Nat)(fill: α): Vector α len := -/ 
-/-   let res := acc.toArray.take len ++ (Array.mkArray (len - n) fill) -/
-/-   ⟨res, by -/ 
-/-     simp [res] -/
-/-     cases h: decide (len > n) <;> simp at * -/
-/-     · simp [Nat.min_eq_left h] -/
-/-       exact Nat.sub_eq_zero_iff_le.mpr h -/
-/-     · simp [Nat.min_eq_right (Nat.le_of_lt h)] -/
-/-       simp [←Nat.add_sub_assoc (Nat.le_of_lt h) n] -/
-/-       simp [Nat.add_sub_cancel_left] -/
-/-     ⟩ -/
-
 def Array.chunks_exact(k: Nat)(arr: Array α): Array (Vector α k) :=
-  assert! arr.size % k == 0
+  /- assert! arr.size % k == 0 -/
   if h: arr.size < k ∨ k = 0 then 
     #[]
   else
@@ -65,7 +50,6 @@ termination_by arr.size
 /- def String.zfill(s: String)(len: Nat) := List.asString <| -/
 /-   (List.replicate (len - s.length) '0') ++ (s.take len).toList -/
 
-
 def UInt8.toBitString(u: UInt8): Vector Bool 8 :=
   let bv := u.toBitVec
   #v[ (bv >>> 0) % 2 == 1,
@@ -98,25 +82,10 @@ def ByteArray.toBitVecBE(arr: ByteArray): BitVec (8*arr.size) :=
 def String.toBitVecLE(s: String) := ByteArray.toBitVecLE <| toUTF8 s
 def String.toBitVecBE(s: String) := ByteArray.toBitVecBE <| toUTF8 s
 
+def BitVec.toList(bv: BitVec n): List Bool := List.finRange n |>.map (bv[·])
 def BitVec.toArray(bv: BitVec n): Array Bool := Array.finRange n |>.map (bv[·])
-def BitVec.ofFn(f: Fin n → Bool): BitVec n := (BitVec.ofBoolListLE <| List.ofFn f).setWidth n
-def BitVec.set(i:Fin n)(b: Bool)(bv: BitVec n): BitVec n := bv ^^^ (((bv[i] ^^ b).toNat : BitVec n) <<< i.val)
-
-/- def Utils.toNat(S: Vector Bool n): Nat := S.mapIdx (2^· * if · then 1 else 0) |>.toList |>.sum -/
-/- #eval Utils.toNat #v[true, false, true, true] -/
-/- #eval BitVec.ofBoolListLE [true, false, true, true] |>.toNat -/
-/- #eval BitVec.ofBoolListLE [true, false, false, false] |>.toNat -/
-/- #eval BitVec.ofBoolListBE [true, false, false, false] |>.toNat -/
-/- #eval 1#1 ++ 0#1 ++ 0#1 ++ 1#1 ++ 0#1 |>.toArray -/
-/- #eval (0b10010#5).toArray -/
-/- #check BitVec.high_truncate -- ??? -/
-
-/- def Utils.toHex (S: Vector Bool n): String := S -/ 
-/-   |> Utils.toNat -/
-/-   |>.toDigits (base := 16) |>.asString -/
-
-/- #eval Utils.toHex #v[true, false, true, true] -/
-/- #eval (1101#4)[1:10] -/
+def BitVec.ofFn(f: Fin n → Bool): BitVec n := (BitVec.ofBoolListLE <| List.ofFn f).cast (List.length_ofFn f)
+def BitVec.set(i: Fin n)(b: Bool)(bv: BitVec n): BitVec n := bv ^^^ (((bv[i] ^^ b).toNat : BitVec n) <<< i.val)
 
 def BitVec.toByteArray(bv: BitVec n): ByteArray :=
   let paddedLen := (n + 7)/8
@@ -151,44 +120,3 @@ def Utils.dump (S: BitVec n)(spacing? : Bool := false): Std.Format :=
       nest 8 <| lineBreak? ++ formatted
     else
       formatted
-
-/- open Std.Format in -/ 
-/- def Utils.dump (S: BitVec n)(spacing? : Bool := false): Std.Format := --S.toNat.toDigits (base := 16) |>.asString -/
-/-     let S: Vector Bool n := ⟨⟨List.finRange n |>.map (S[·])⟩, by simp⟩ -/
-/-     let width := 8 -/
-/-     let singleSpace? := if spacing? then text " " else nil -/
-/-     let lineBreak? := if spacing? then align false else nil -/
-
-/-     let final := S.adjust (width * ((S.size + width-1)/width)) false -/
-/-       |>.chunks_exact width -/
-/-       |>.map (fun lane => Utils.toHex lane |>.zfill (width/4)) -/
-/-       |>.toList -/
-/-       |> "".intercalate -/
-/-       |>.zfill ((S.size + 3) / 4) -/
-/-     let formatted := Array.mk final.toList -/
-/-       |>.chunks 16 -/
-/-       |>.map (·.chunks 2 -/ 
-/-         |>.map (fun v => text  <| v.toList.asString) -/ 
-/-         |>.toList -/ 
-/-         |> singleSpace?.joinSep -/
-/-         |> group) -/
-/-       |>.chunks 2 -/
-/-       |>.map (·.toList |> singleSpace?.joinSep) -/
-/-       |>.toList -/
-/-       |> lineBreak?.joinSep -/
-/-     if spacing? then -/ 
-/-       nest 8 <| lineBreak? ++ formatted -/
-/-     else -/
-/-       formatted -/
-
-/- #eval Utils.dump (0b10010011#8) -/
-/- #eval Utils.dump' (0b10010011#8) -/
-
-/- #eval Utils.dump (0b1001001110101111#16) -/
-/- #eval Utils.dump' (0b1001001110101111#16) -/
-
-/- #eval Utils.dump (0b100100111010111101001011#24) -/
-/- #eval Utils.dump' (0b100100111010111101001011#24) -/
-
-/- #eval Utils.dump (0b10010011101011110100101100001000#22) -/
-/- #eval Utils.dump' (0b10010011101011110100101100001000#32) -/
